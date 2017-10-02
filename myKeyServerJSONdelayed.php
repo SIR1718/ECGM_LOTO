84ed020429d30620d0b65c7a281295fa81f5062c
pedromoreira-estg <pmoreira.estg@gmail.com>
2017-10-02 17:57:36 +0100

    ajaxc client

---
+++ myKeyServerJSONdelayed.php
@@ -0,0 +1,52 @@
+<?php 
+//class in php
+
+class KeyGenerator {
+
+public $numbers;
+public $stars;
+
+public function __construct($n,$s) {
+
+	$this->numbers = array();
+	$this->stars = array();
+
+	// numbers
+
+	do {
+		$randomNumber = rand(1,50);
+		if (!in_array($randomNumber,$this->numbers)) {
+			$this->numbers[] = $randomNumber;
+		}
+	} while (count($this->numbers) < $n);
+	sort($this->numbers);
+
+	// stars
+
+	do {
+		$randomNumber = rand(1,11);
+		if (!in_array($randomNumber,$this->stars)) {
+			$this->stars[] = $randomNumber;
+		}
+	} while (count($this->stars) < $s);
+	sort($this->stars);
+
+}
+
+}
+
+
+// new instance
+$k = new KeyGenerator(5,3);
+
+$JSONKey = json_encode($k);
+
+
+// dump the object
+//var_dump($k);
+
+sleep(5);
+
+echo $JSONKey;
+
+?>
