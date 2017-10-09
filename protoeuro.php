1df046782554c8f7303427c9496adf694d9522e7
pedromoreira-estg <pmoreira.estg@gmail.com>
2017-10-09 16:47:35 +0100

    start class 09.10.2017

--- protoeuro.php
+++ protoeuro.php
@@ -1,7 +1,7 @@
 <?php
 //$textJSON = file_get_contents("http://62.28.241.149/~pmoreira/KEYSERVER/KeyServerJSON.php");
 
-$textJSON = file_get_contents("http://localhost/SIR1718/LOTO_D/keyServerJSON.php");
+$textJSON = file_get_contents("http://localhost/SIR1718/LOTO_ECGM/myKeyServerJSON.php");
 
 $varPHP = json_decode($textJSON);
 
